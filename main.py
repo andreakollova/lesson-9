9f91c0395fb5b28592e5e8f6a3602b0e9dcd8a14
andreakollova <andrea_kollova@hotmail.com>
2025-01-28 19:08:22 +0100

    Pridal som svoj print s mojim menom do main.py

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-nazov_suboru = input("Zadaj názov súboru, do ktorého chceš zapísať svoj text:")
-tvoj_text = input("Zadaj svoj text:")
-
-nazov_suboru = open(nazov_suboru, "w")
-nazov_suboru.writelines(tvoj_text)
-
-nazov_suboru.close()
+print("Ahoj Patrik")
